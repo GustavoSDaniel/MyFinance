1dcf730bc081554b3c96d5aedd36dcef19dc41d2
GustavoSDaniel <gustavosdaniel@hotmail.com>
2025-12-27 17:30:03 -0300

    feat: configurando o application e o docker-compose

---
+++ myfinance-api/src/main/java/com/gustavosdaniel/myfinance_api/config/CacheConfig.java
@@ -0,0 +1,8 @@
+package com.gustavosdaniel.myfinance_api.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CacheConfig {
+
+}
--- myfinance-api/src/main/java/com/gustavosdaniel/myfinance_api/config/SecurityConfig.java
+++ myfinance-api/src/main/java/com/gustavosdaniel/myfinance_api/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
     };
 
     @Bean
-    public SecurityFilterChain springSecurityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
                 .csrf(AbstractHttpConfigurer::disable)
